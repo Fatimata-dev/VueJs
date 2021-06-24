7cb682fa615a26263f1e811c84663ab4942824f4
Fatimata-dev <dramefatim7@gmail.com>
2021-06-24 12:38:43 +0200

    exercice sur les fimls avec la cli

---
+++ 16-exo/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Exercice</title>
+        
+    </head>
+    <body>
+        <div id="app">
+            <the-header @update-recherche="updateRecherche"></the-header>
+            <the-body :rechercher="recherche"></the-body>
+        </div>
+    </body>
+</html>
+
+</template>
+
+<script>
+
+    import TheHeader from './components/Header.vue';
+    import TheBody from './components/Body.vue';
+
+
+    const App =  {
+        components: {
+            // 'the-header': TheHeader,
+            // 'the-body': TheBody
+            TheHeader,
+            TheBody
+        },
+        data() {
+            return {
+                recherche: ''
+            }
+        },
+        methods:{
+            updateRecherche(event) {
+                console.log("Recherche: " + event);
+                this.recherche = event;
+            }
+        }
+    }
+export default App ;
+</script>
+
+<style>
+/* vbase pour recuperer les la structure de base */
+</style>
---
+++ 16-exo/src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+     <header>
+            <h1>Super Site</h1>
+            <the-navigation-header @envoyer-recherche="$emit('update-recherche', $event)">
+            </the-navigation-header>
+        </header>
+</template>
+<script>
+
+
+import TheNavigationHeader from './TheNavigation.vue';
+const Header = {
+    components: {
+        'the-navigation-header': TheNavigationHeader
+    }
+}
+export default Header;
+</script>
+<style >
+    header{
+        display: flex;
+        color: white;
+    }
+</style>
+
+
+
+
