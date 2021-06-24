b422db00f488a71e7cebb3812142a4427bd3c436
Fatimata-dev <dramefatim7@gmail.com>
2021-06-24 11:14:43 +0200

    vue?js en cli

---
+++ 15-cli/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-title></v-title>
+  </div>
+</template>
+
+<script>
+import Title from './components/Title.vue'
+let appComponent = {
+  components:{
+    'v-title': Title
+  }
+}
+export default appComponent;
+</script>
+
+<style>
+
+</style>
---
+++ 15-cli/src/components/Title.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Hello Douna !!!!</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum quae ut eos veniam deserunt laudantium sit, voluptatem, numquam libero, omnis impedit maiores sequi! Adipisci iure, magnam hic ipsam vero incidunt?</p>
+
+    </div>
+</template>
+
+<script>
+const TitleComponenent = {
+    data: function(){
+        return {
+
+        }
+        
+    },
+    methods: {
+
+        }
+}
+export default TitleComponenent;
+</script>
+<!-- scoped = le style concerne seulement le template indiqué -->
+<style scoped>
+    h1 {
+        color: cornflowerblue;
+    }
+</style>
