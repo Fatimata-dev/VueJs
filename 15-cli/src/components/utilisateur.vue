e2920abe595f3855ca92983b76ec67dda9365cf4
Fatimata-dev <dramefatim7@gmail.com>
2021-06-25 09:52:44 +0200

    axios utilisation des API

--- 15-cli/src/components/utilisateur.vue
+++ 15-cli/src/components/utilisateur.vue
@@ -5,8 +5,21 @@
         <ul>
             <li v-for="utilisateur of utilisateurs" :key="utilisateur.id">
                 {{ utilisateur.name }}
+                {{ utilisateur.username }}
             </li>
         </ul>
+        <form @submit.prevent="addArticle">
+            <!-- <label for="identifiant">Id</label>
+            <input type="number" name="identifiant"  id="identifiant"> -->
+            <label for="titre">Titre</label>
+            <input type="text" name="titre"  id="titre">
+
+            <label for="msg">Message</label>
+            <input type="text" name="msg"  id="msg">
+            <input type="submit" value="Valider">
+            <p v-if="newArticle">L'article n°{{ newArticle.id }} à bien été créer</p>
+            
+        </form>
     </div>
 </template>
 
@@ -15,7 +28,8 @@ import axios from 'axios';
     export default {
         data() {
             return {
-                utilisateurs: []
+                utilisateurs: [],
+                newArticle: null
             }
         },
         methods: {
@@ -28,11 +42,30 @@ import axios from 'axios';
                     }
                 );
                 
+            },
+           async addArticle(event){
+                const titre = event.target.titre.value;
+                const message = event.target.msg.value;
+                const userId = 1;
+
+                const article = {
+                    userId,
+                    title: titre,
+                    body: message
+                }
+                // Dans une fonction asyn, on peut utiliser await pour recuperer la reponse d'une promesse
+                const reponse = await axios.post('https://jsonplaceholder.typicode.com/posts',
+                article);
+                
+                if (reponse.status == 201) {
+                    this.newArticle = reponse.data;
+                }
+                console.log(reponse);
             }
         },
     }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
